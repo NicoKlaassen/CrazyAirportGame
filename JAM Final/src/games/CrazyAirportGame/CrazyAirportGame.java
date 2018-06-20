d2ab0124c3b6fbb88193909764a7de0b7efeda93
Nico <Nico@192.168.0.81>
2018-06-20 12:23:02 +0200

    Projekt weiter angepasst

--- JAM Final/src/games/CrazyAirportGame/CrazyAirportGame.java
+++ JAM Final/src/games/CrazyAirportGame/CrazyAirportGame.java
@@ -1,7 +1,97 @@
 package games.CrazyAirportGame;
 
 import gameClasses.Game;
+import gameClasses.GameState;
+import global.FileHelper;
+import userManagement.User;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class CrazyAirportGame extends Game{
+
+	@Override
+	public String getSite() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCSS() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getJavaScript() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getMaxPlayerAmount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getCurrentPlayerAmount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void execute(User user, String s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<User> getPlayerList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<User> getSpectatorList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getGameData(String eventName, User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addSpectator(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isJoinable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void playerLeft(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public GameState getGameState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
