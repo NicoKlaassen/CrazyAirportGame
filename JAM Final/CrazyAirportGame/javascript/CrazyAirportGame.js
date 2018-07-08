f41d2d6155ac8b568d3e9671272b941aa374e94e
Martinm254 <33454066+Martinm254@users.noreply.github.com>
2018-07-08 23:00:32 +0200

    Teilprojekte, CSS und erstes Javascript

--- JAM Final/CrazyAirportGame/javascript/CrazyAirportGame.js
+++ JAM Final/CrazyAirportGame/javascript/CrazyAirportGame.js
@@ -1,7 +1,19 @@
 
 
 function init() {
-    console.log("loeschen");
+    console.log("loeschen der kopfzeile");
     var elem = document.getElementById('menu');
     elem.parentNode.removeChild(elem);
+}
+
+function roll() {
+    console.log("wuerfel rollen");
+    if(document.getElementById("projects").style.display=='none'){
+        document.getElementById("projects").style.display='block';
+        document.getElementById("diceText").style.display='block';
+    }
+    else{
+        document.getElementById("projects").style.display='none';
+        document.getElementById("diceText").style.display='none';
+    }
 }
